e01c64ec67599651782ffe0902d165e1bba5da68
Chris Torres <hwct83@gmail.com>
2019-09-11 17:16:03 -0700

    added js code to pull JSON data into HTML

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 // write your code here
-
+    $.getJSON("data.json", function(data) {
+        var spot_data = '';
+        $.each(data, function(key, value){
+            spot_data += '<tr>';
+            spot_data += '<td>'+value.name+'</td>';
+            spot_data += '<td>'+value.description+'</td>';
+            spot_data += '<td>'+'<a href="https://www.google.com/maps?q='+value.location+'">'+'<button>'+'Open in Google Maps'+'</button>'+'</a>'+'</td>';
+            spot_data += '</tr>';
+        });
+        $('#spot_table').append(spot_data);
+    });
 });
